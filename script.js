7176adaf3722be890f93286d8539fd6e230928c7
daniaedu <rocketbookdania@gmail.com>
2023-09-29 21:46:15 -0500

    Add JavaScript for grid generation and grid styling

---
+++ script.js
@@ -0,0 +1,27 @@
+const container = document.getElementById('container');
+const colorButton = document.getElementById('colorMode');
+const rainbowButton = document.getElementById('rainbowMode');
+const eraserButton = document.getElementById('eraser');
+
+// Fixed grid size (N x N) 
+const N = 8;
+
+// Nested loops to create grid squares within the container
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
+    // Calculate the width of each grid square based on the grid size (N)
+    const flexValue = 100 / N;
+
+    // Create a new div element for the grid square
+    const gridSquare = document.createElement('div');
+
+    // Add the 'grid' class to the grid square for styling
+    gridSquare.classList.add('gridSquare');
+
+    // Set the flex property to control the width of the grid square
+    gridSquare.style.flex = `0 0 ${flexValue}%`;
+
+    // Append the grid square to the container div
+    container.appendChild(gridSquare);
+  }
+}
